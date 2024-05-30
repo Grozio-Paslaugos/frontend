baedf7cc7eedc13b23340958ee27f91f6031ae13
KaLa <k.lapienis@gmail.com>
2024-05-30 17:36:51 +0300

    yes

--- src/Components/MainLayout/MainLayout.jsx
+++ src/Components/MainLayout/MainLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 
 import Header from "../Header/Header";
-import UserProceduresList from "../UserProcedureList/UserProceduresList";
 
 export default function MainLayout() {
   return (
